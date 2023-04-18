9d3330e2ac70a45b76f0b173237bb93ccb6c0882
Simon Prickett <simon@redislabs.com>
2023-04-18 13:50:11 +0100

    Added some comments and better 404.

--- server/app.py
+++ server/app.py
@@ -13,16 +13,19 @@ redis_client = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 # Retrieve an image from Redis.
 @app.route("/image/<image_id>")
 def get_image(image_id):
+    # Look for the image data in Redis.
     image_data = redis_client.get(f"{IMAGE_KEY_PREFIX}:{image_id}")
 
     if image_data is None:
-        return "Not found.", 404
+        return f"Image {image_id} not found.", 404
     
+    # Load up the data into a file like data structure.
     image_file = io.BytesIO(image_data)
 
     # Rewind the image file to the start...
     image_file.seek(0)
 
+    # TODO get the mime type from Redis?
     return send_file(image_file, mimetype="image/jpeg")
 
 @app.route("/")
