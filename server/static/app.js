6ccc27d3ce8fe1ec73eca14896357acbd7c750ff
Simon Prickett <simon@redislabs.com>
2023-04-18 19:58:44 +0100

    Sorted a front end out a bit more.

---
+++ server/static/app.js
@@ -0,0 +1,59 @@
+const API_PREFIX = 'api';
+
+// Load image data from the backend...
+async function renderImages() {
+  function renderImageData(dataItems) {
+    let html = '';
+    for (const k in dataItems) {
+      html = `${html}<li><span class="has-text-weight-bold">${k}:</span> ${dataItems[k]}</li>`;
+    }
+
+    return html;
+  }
+  const imageResponse = await fetch(`/${API_PREFIX}/images`);
+  const imageIds = await imageResponse.json();
+
+  if (imageIds.length === 0) {
+    // Unhide the notification
+    const noImagesNotification = document.getElementById('noImagesNotification');
+    noImagesNotification.classList.remove('is-hidden');
+    return;
+  }
+
+  const imageArea = document.getElementById('imageArea');
+
+  for (const imageId of imageIds) {
+    const imageDetailResponse = await fetch(`${API_PREFIX}/data/${imageId}`)
+    const imageData = await imageDetailResponse.json();
+    const imageUrl = `/${API_PREFIX}/image/${imageId}`;
+    console.log(imageData);
+    console.log(imageUrl);
+
+    const imageHTML = `
+      <div class="card m-4">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="${imageUrl}" alt="Image ${imageId}">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">${new Date(parseInt(imageId * 1000, 10)).toUTCString()}</p>
+            </div>
+          </div>
+    
+          <div class="content">
+            <ul>
+              ${renderImageData(imageData)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+    imageArea.innerHTML = `${imageArea.innerHTML}${imageHTML}`;
+  }
+}
+
+renderImages();
