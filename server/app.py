d08c263376567d49d696804afe9db25ac603ea1f
Simon Prickett <simon@redislabs.com>
2023-07-04 17:12:39 +0100

    JSON server.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask, render_template, send_file
+import base64
 import io
 import os
 import redis
@@ -45,9 +46,9 @@ def get_all_images():
     for doc in search_results.docs:
         this_image = dict()
         this_image[IMAGE_ID_FIELD_NAME] = doc.id.removeprefix(f"{IMAGE_KEY_PREFIX}:")
-        this_image[IMAGE_TIMESTAMP_FIELD_NAME] = doc.timestamp
+        this_image[IMAGE_TIMESTAMP_FIELD_NAME] = int(doc.timestamp)
         this_image[IMAGE_MIME_TYPE_FIELD_NAME] = doc.mime_type
-        this_image[IMAGE_LUX_FIELD_NAME] = doc.lux
+        this_image[IMAGE_LUX_FIELD_NAME] = int(doc.lux)
         all_images.append(this_image)
 
     return all_images
@@ -56,19 +57,21 @@ def get_all_images():
 @app.route(f"/{API_ROUTE_PREFIX}/image/<image_id>")
 def get_image(image_id):
     # Look for the image data in Redis.
-    image_data = redis_client.hmget(f"{IMAGE_KEY_PREFIX}:{image_id}", [ IMAGE_DATA_FIELD_NAME, IMAGE_MIME_TYPE_FIELD_NAME ])
+    image_data = redis_client.json().get(f"{IMAGE_KEY_PREFIX}:{image_id}", "$.image_data", "$.mime_type")
 
-    if image_data[0] is None:
+    print(image_data)
+
+    if image_data is None:
         return f"Image {image_id} not found.", 404
     
     # Load up the data into a file like data structure.
-    image_file = io.BytesIO(image_data[0])
+    image_file = io.BytesIO(base64.b64decode(image_data["$.image_data"][0]))
 
     # Rewind the image file to the start...
     image_file.seek(0)
 
     # Get the MIME type from the Redis response, and decode it from binary.
-    return send_file(image_file, mimetype=image_data[1].decode(STRING_ENCODING))
+    return send_file(image_file, mimetype=image_data["$.mime_type"][0])
 
 @app.route("/")
 def home():
